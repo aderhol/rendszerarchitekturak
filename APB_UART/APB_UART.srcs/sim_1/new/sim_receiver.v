80a94f1a377f398f7727812ab57bee4f204c2151
adam <a.e.hollos@gmail.com>
2019-05-07 23:54:01 +0200

    receiver finished

---
+++ APB_UART/APB_UART.srcs/sim_1/new/sim_receiver.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+module sim_receiver(
+
+    );
+    
+    reg clk, rst, next;
+    wire [7:0] data;
+    wire [6:0] data_7;
+    assign data_7 = data[6:0];
+    reg [7:0] D;
+    reg D_next;
+    wire line;
+    transmitter tr (
+        .clk(clk),
+        .rst(rst),
+        .N(16'd3),
+        .eight_bit(1'b0),
+        .two_stop(0),
+        .data(D),
+        .next(D_next),
+        .Tx(line)
+        );
+        
+    receiver uut(
+        .clk(clk),
+        .rst(rst),
+        .N(16'd3), 
+        .eight_bit(1'b0),
+        .Rx(line),
+        .data(data),
+        .next(next)
+        );
+    
+    
+    
+    
+    initial begin
+        rst <= 1;
+        clk <= 0;
+        next <= 0;
+        D_next <= 0;
+        
+        #11
+        rst <= 0;
+        
+        #5
+        D <= 8'd77;
+        D_next <= 1'b1;
+        
+        #10
+        D_next = 1'b0;
+        
+        #10000
+        next <= 1'b1;
+        D <= 8'd101;
+        D_next <= 1'b1;
+        #10
+        next <= 1'b0;
+        D <= 8'd33;
+        D_next <= 1'b1;
+        #10
+        D_next <= 1'b0;
+        
+        #6000
+        next <= 1'b1;
+        #10
+        next <= 1'b0;
+    end
+        
+    always #5
+        clk = ~clk;
+endmodule
---
+++ APB_UART/APB_UART.srcs/sources_1/new/receiver.v
@@ -0,0 +1,105 @@
+`timescale 1ns / 1ps
+
+module receiver(
+    input clk,
+    input rst,
+    input [15:0] N, //f_sampling = F_osc / N = 16 * baud
+    input eight_bit,
+    input Rx,
+    output [7:0] data,
+    input next,
+    output empty,
+    output full
+    );
+        
+    wire SClk;
+    reg [15:0] SClk_count;
+    assign SClk = (N == 16'd1) ? clk : (SClk_count == N-1);
+    always @(posedge clk) begin
+        if(rst)
+            SClk_count <= 0;
+        else if(SClk)
+             SClk_count <= 0;
+        else
+            SClk_count <= SClk_count + 1;            
+    end
+    
+    reg sample;
+    always @(posedge clk)
+        if(SClk)
+            sample <= Rx;
+    
+    reg buff[1:0];
+    wire dec;
+    assign dec = ((sample + buff[0] + buff[1]) >= 2'd2); 
+    always @(posedge clk) begin
+        if(SClk) begin
+            buff[0] <= buff[1];
+            buff[1] <= sample;
+        end
+    end
+    
+    reg [7:0] shMem;
+    reg rec;
+    FIFO #(.depth(32)) mem(
+        .clk(clk),
+        .rst(rst),
+        .in(shMem),
+        .in_next(rec),
+        .out(data),
+        .out_next(next),
+        .empty(empty),
+        .full(full)
+        );
+    
+       
+    reg [3:0] state; //0:wait, 1:start bit, 2-:data bits
+    reg [3:0] cnt;
+    always @(posedge clk) begin
+        if(rst) begin
+            state <= 4'd0;
+            rec <= 1'b0;
+        end
+        else if(SClk) begin
+            if(state == 4'd0) begin
+                if(sample == 1'b0) begin //start condition
+                    state <= 4'd1;
+                    cnt <= 4'd0;
+                end
+			end
+            else if(state == 4'd1) begin
+                if(sample == 1'b1)
+                    state <= 4'd0;
+                else begin
+                    if(cnt == 7) begin //if the start condition lasted for half a bit time
+                        cnt <= 4'd0;
+                        state <= state + 1;
+                    end
+                    else
+                        cnt <= cnt + 1;
+                end
+            end
+            else if(state == (4'd8 + eight_bit)) begin //last bit
+                if(cnt == 4'd15) begin
+                    state <= 4'd0;
+                    shMem[state - 4'd2] <= dec;
+                    rec <= 1'b1;
+                end
+                else
+                    cnt <= cnt + 1;
+            end
+            else begin
+                if(cnt == 4'd15) begin
+                    cnt <= 4'd0;
+                    state <= state + 4'd1;
+                    shMem[state - 4'd2] <= dec;
+                end
+                else
+                    cnt <= cnt + 1;
+            end
+        end
+        else
+            rec <= 1'b0;
+    end
+    
+endmodule
